4ac9a89841766eac2ae88a41d43fe530c1cada7c
Sploder12 <trevorschupbach@gmail.com>
2024-02-12 21:35:39 -0500

    Create game.py

---
+++ src/game/game.py
@@ -0,0 +1,43 @@
+import pygame
+
+
+def handleMouseMove(pos, rel, buttons, touch):
+    pass
+
+def handleMousePress(pos, button, touch):
+    pass
+
+def gameloop(screen):
+    
+    screen.fill((10, 13, 15))
+
+    # Put rendering code here
+
+    pygame.display.flip()
+
+    for event in pygame.event.get():
+        if (event.type == pygame.QUIT):
+            return False
+        elif (event.type == pygame.MOUSEMOTION):
+            handleMouseMove(event.pos, event.rel, event.buttons, event.touch)
+        elif (event.type == pygame.MOUSEBUTTONDOWN):
+            handleMousePress(event.pos, event.button, event.touch)
+
+    return True
+
+def main():
+    
+    screen = pygame.display.set_mode((1280, 720))
+    pygame.display.set_caption("We Love The Company.")
+
+    while gameloop(screen):
+        pass
+
+
+if __name__ == "__main__":
+    pygame.init()
+
+    main()
+
+    pygame.quit()
+
