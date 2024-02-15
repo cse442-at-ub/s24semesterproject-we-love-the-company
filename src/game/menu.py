61fc5b0e330af6f92e05d03d850a57276e02c769
Sayaf1231 <uaretheone55@gmail.com>
2024-02-15 17:33:53 -0500

    This is the new version has a options quite a few changes i think

--- src/game/menu.py
+++ src/game/menu.py
@@ -1,13 +1,15 @@
 import pygame
 import os
+from OptionsMenu import Settings
+from Buttons import Button
+#This initialization thing I think we did it in the game.py so I think we can use that and not
+#use this makes it game.py pointless kinda. tbh im getting used to pygame so idk for sure tho.
 
 # Initialize Pygame
 pygame.init()
 
 # Set up the path for assets
 path = os.path.dirname(__file__) + "/"
-print(path)
-
 # Initialize global variables
 current_connection = {}
 currently_connected = 0
@@ -15,7 +17,6 @@ friends = []
 
 # Set up the window
 window = pygame.display.set_mode((1024, 720))
-pygame.display.set_caption("ShadeShare")
 
 # Set up game variables
 newVolume = 0.5
@@ -23,45 +24,18 @@ clock = pygame.time.Clock()
 textFont = pygame.font.SysFont("Arial", 40)
 game_won = 0
 
-class Button():
-    def __init__(self, image, pos, text_input, font, base_color, hovering_color):
-        self.image = image
-        self.x_pos = pos[0]
-        self.y_pos = pos[1]
-        self.font = font
-        self.base_color, self.hovering_color = base_color, hovering_color
-        self.text_input = text_input
-        self.text = self.font.render(self.text_input, True, self.base_color)
-        if self.image is None:
-            self.image = self.text
-        self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
-        self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
-
-    def update(self, screen):
-        if self.image is not None:
-            screen.blit(self.image, self.rect)
-        screen.blit(self.text, self.text_rect)
-
-    def checkForInput(self, position):
-        if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
-            return True
-        return False
-
-    def changeColor(self, position):
-        if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
-            self.text = self.font.render(self.text_input, True, self.hovering_color)
-        else:
-            self.text = self.font.render(self.text_input, True, self.base_color)
 
 def draw_text(text, font, color, x, y):
     textImg = font.render(text, True, color)
     window.blit(textImg, (x, y))
 
 def mainMenu():
-    pygame.display.set_caption("background")
+    pygame.display.set_caption("We love the Company")
     run = True
     window_size = window.get_size()  # Get the current window size
 
+    #intializing the options object so I can call it later (designated by : here)
+    Options = Settings(window, clock, path, textFont)
 
     while run:
         clock.tick(27)  # Control framerates
@@ -77,14 +51,20 @@ def mainMenu():
         screen_center_x = window.get_width() // 2
         play_button_y = window.get_height() // 2 - 50
         exit_button_y = window.get_height() // 2 + 150
+        #putting the settings button in the middle of the play and exit buttons
+        settings_button_y = window.get_height() // 2 + 50
 
         PlayButton = Button(image=pygame.image.load(path + "Assets/button.png"), pos=(screen_center_x, play_button_y),
                             text_input="Play", font=textFont, base_color="white", hovering_color="blue")
 
         ExitButton = Button(image=pygame.image.load(path + "Assets/button.png"), pos=(screen_center_x, exit_button_y),
                             text_input="Exit", font=textFont, base_color="white", hovering_color="blue")
+        
+        #put a settings button with the button.img. It uses the same fonts and color and hover color
+        SettingsButton = Button(image=pygame.image.load(path + "Assets/button.png"), pos=(screen_center_x, settings_button_y),
+                                text_input="Settings", font=textFont, base_color="white", hovering_color="blue")
 
-        for button in [PlayButton, ExitButton]:
+        for button in [PlayButton, ExitButton, SettingsButton]:
             button.changeColor(menuMousePOS)
             button.update(window)
 
@@ -94,8 +74,16 @@ def mainMenu():
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if PlayButton.checkForInput(menuMousePOS):
                     print("Play button clicked")  # Placeholder for starting the game
-                if ExitButton.checkForInput(menuMousePOS):
+                elif ExitButton.checkForInput(menuMousePOS):
                     run = False
+                #if the settings button is clicked then it should say settinsg button clicked in the terminal an take to the options menu
+                elif SettingsButton.checkForInput(menuMousePOS):
+                    print("Settings button clicked")
+                    #calling the settings page func from the settings class (in @OptionsMenu)
+                    #here
+                    Options.display_settings_menu()
+                    
+                
 
         pygame.display.update()
 
