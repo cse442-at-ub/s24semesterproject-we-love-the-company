e30376ef7da344292c0688f8a0b24b74c2cceb37
Sploder12 <trevorschupbach@gmail.com>
2024-02-20 23:50:10 -0500

    refactor part1
    yay we have to rewrite all the menus to make them even remotely possible to test!

--- src/game/menu.py
+++ src/game/menu.py
@@ -1,107 +1,80 @@
 import pygame
 import os
+
+import gamestate
+
 from OptionsMenu import Settings
 from Buttons import Button
 from HowToPlay import Instructions
-#This initialization thing I think we did it in the game.py so I think we can use that and not
-#use this makes it game.py pointless kinda. tbh im getting used to pygame so idk for sure tho.
 
-# Initialize Pygame
-pygame.init()
+ID = "main_menu"
 
-# Set up the path for assets
-path = os.path.dirname(__file__) + "/"
-# Initialize global variables
-current_connection = {}
-currently_connected = 0
-friends = []
+class MenuScene:
+    def __init__(self, screen):
+        self.id = ID
 
-# Set up the window
-window = pygame.display.set_mode((1024, 720))
+        self.path = os.path.dirname(__file__) + "/"
 
-# Set up game variables
-newVolume = 0.5
-clock = pygame.time.Clock()
-textFont = pygame.font.SysFont("Arial", 40)
-paragraphFont = pygame.font.SysFont("Arial", 30)
-game_won = 0
+        clock = pygame.time.Clock()
 
+        self.textFont = pygame.font.SysFont("Arial", 40)
+        self.paragraphFont = pygame.font.SysFont("Arial", 30)
 
-def draw_text(text, font, color, x, y):
-    textImg = font.render(text, True, color)
-    window.blit(textImg, (x, y))
+        #intializing the options object so I can call it later (designated by : here)
+        self.Options = Settings(screen, clock, self.path, self.textFont)
 
-def mainMenu():
-    pygame.display.set_caption("We love the Company")
-    run = True
-    window_size = window.get_size()  # Get the current window size
+        #initialize How To Play screen here
+        self.HowToPlay = Instructions(screen, clock, self.path, self.textFont, self.paragraphFont)
 
-    #intializing the options object so I can call it later (designated by : here)
-    Options = Settings(window, clock, path, textFont)
-
-    #initialize How To Play screen here
-    HowToPlay = Instructions(window, clock, path, textFont, paragraphFont)
-
-    while run:
-        # WHY 27 FPS??? ? ??? ????????
-        clock.tick(27)  # Control framerates
-        window.fill((0, 0, 0))
-
-        # the slowest thing you could possibly do
-        background_image = pygame.image.load(path + 'background.jpg')
-        background_image = pygame.transform.scale(background_image, window_size)
-        window.blit(background_image, (0, 0))
-
-
-
-        menuMousePOS = pygame.mouse.get_pos()
-
-        screen_center_x = window.get_width() // 2
-        play_button_y = window.get_height() // 2 - 50
-        exit_button_y = window.get_height() // 2 + 150
+        screen_center_x = screen.get_width() // 2
+        play_button_y = screen.get_height() // 2 - 50
+        exit_button_y = screen.get_height() // 2 + 150
         #putting the settings button in the middle of the play and exit buttons
-        settings_button_y = window.get_height() // 2 + 50
-        instruct_button_y = window.get_height() - 50
+        settings_button_y = screen.get_height() // 2 + 50
+        instruct_button_y = screen.get_height() - 50
 
-        # the slowest thing you could possibly do pt2
-        PlayButton = Button(image=pygame.image.load(path + "Assets/button.png"), pos=(screen_center_x, play_button_y),
-                            text_input="Play", font=textFont, base_color="white", hovering_color="blue")
+         # the slowest thing you could possibly do pt2
+        self.PlayButton = Button(image=pygame.image.load(self.path + "Assets/button.png"), pos=(screen_center_x, play_button_y),
+                            text_input="Play", font=self.textFont, base_color="white", hovering_color="blue")
 
-        ExitButton = Button(image=pygame.image.load(path + "Assets/button.png"), pos=(screen_center_x, exit_button_y),
-                            text_input="Exit", font=textFont, base_color="white", hovering_color="blue")
+        self.ExitButton = Button(image=pygame.image.load(self.path + "Assets/button.png"), pos=(screen_center_x, exit_button_y),
+                            text_input="Exit", font=self.textFont, base_color="white", hovering_color="blue")
         
         #put a settings button with the button.img. It uses the same fonts and color and hover color
-        SettingsButton = Button(image=pygame.image.load(path + "Assets/button.png"), pos=(screen_center_x, settings_button_y),
-                                text_input="Settings", font=textFont, base_color="white", hovering_color="blue")
+        self.SettingsButton = Button(image=pygame.image.load(self.path + "Assets/button.png"), pos=(screen_center_x, settings_button_y),
+                                text_input="Settings", font=self.textFont, base_color="white", hovering_color="blue")
 
-        InstructionsButton = Button(image=pygame.image.load(path + "Assets/button.png"), pos=(screen_center_x, instruct_button_y),
-                                text_input="How To Play", font=textFont, base_color="white", hovering_color="blue")
+        self.InstructionsButton = Button(image=pygame.image.load(self.path + "Assets/button.png"), pos=(screen_center_x, instruct_button_y),
+                                text_input="How To Play", font=self.textFont, base_color="white", hovering_color="blue")
 
-        for button in [PlayButton, ExitButton, SettingsButton, InstructionsButton]:
-            button.changeColor(menuMousePOS)
-            button.update(window)
+        self.buttons = [self.PlayButton, self.ExitButton, self.SettingsButton, self.InstructionsButton]
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                if PlayButton.checkForInput(menuMousePOS):
-                    print("Play button clicked")  # Placeholder for starting the game
-                elif ExitButton.checkForInput(menuMousePOS):
-                    run = False
-                #if the settings button is clicked then it should say settinsg button clicked in the terminal an take to the options menu
-                elif SettingsButton.checkForInput(menuMousePOS):
-                    print("Settings button clicked")
-                    #calling the settings page func from the settings class (in @OptionsMenu)
-                    #here
-                    Options.display_settings_menu()
-                elif InstructionsButton.checkForInput(menuMousePOS):
-                    print("How To Play button clicked")
-                    HowToPlay.display_instructions_menu()
+    def initHandlers(self, state: gamestate.Gamestate):
+        state.handlers[ID] = gamestate.Handler(render, gamestate.doNothing, gamestate.doNothing, mouseMove, mousePress)
 
-        pygame.display.update()
+def mouseMove(state: gamestate.Gamestate, pos, rel, buttons, touch):
+    for button in state.scene.buttons:
+        button.changeColor(pos)
 
-    pygame.quit()
+def mousePress(state: gamestate.Gamestate, pos, buttons, touch):
+    if (state.scene.PlayButton.checkForInput(pos)):
+        print("Play button clicked")
+    elif (state.scene.ExitButton.checkForInput(pos)):
+        state.running = False
+        return
+    elif (state.scene.SettingsButton.checkForInput(pos)):
+        print("Settings button clicked")
+        state.scene.Options.display_settings_menu()
+    elif (state.scene.InstructionsButton.checkForInput(pos)):
+        print("How To Play button clicked")
+        state.scene.HowToPlay.display_instructions_menu()
 
-if __name__ == "__main__":
-    mainMenu()
+def render(state: gamestate.Gamestate):
+
+    # the slowest thing you could possibly do
+    background_image = pygame.image.load(state.scene.path + 'background.jpg')
+    background_image = pygame.transform.scale(background_image, state.screen.get_size())
+    state.screen.blit(background_image, (0, 0))
+
+    for button in state.scene.buttons:
+        button.update(state.screen)
