0b6a3ef69032a9762ce9b2fc703fb0a9ffd0a160
ululu <mtmerlino@gmail.com>
2024-02-24 00:35:56 -0500

    grid unit tests

--- src/game/grid.py
+++ src/game/grid.py
@@ -17,6 +17,15 @@ class Grid:
         """Checks if the given pair of coordinates are inside the bounds of the grid."""
         return x >= 0 and y >= 0 and x < self.width and y < self.height
     
+    def get_object(self, x: int, y: int):
+        """Returns the object at the specified coordinates."""
+        """If there is empty space, None is returned instead."""
+
+        if self.matrix[y][x] == EMPTY_SPACE:
+            return None
+        else:
+            return self.matrix[y][x]
+    
     def insert(self, item: dict, x: int, y: int):
         """Places an object at the given coordinates."""
         """Returns True if the placement was successful, and False if something is already in that space."""
@@ -25,8 +34,8 @@ class Grid:
         elif not self.is_inbounds(x,y):
             raise ValueError(f"Attempting to insert item '{item}' at out of bounds grid position ({x},{y}).")
         
-        if self.matrix[x][y] == EMPTY_SPACE:
-            self.matrix[x][y] = item
+        if self.matrix[y][x] == EMPTY_SPACE:
+            self.matrix[y][x] = item
             return True
         else:
             return False
@@ -37,38 +46,38 @@ class Grid:
         if not self.is_inbounds(x,y):
             raise ValueError(f"Attempting to remove item at out of bounds grid position ({x},{y}).")
         
-        removed_item = self.matrix[x][y]
+        removed_item = self.matrix[y][x]
         if removed_item == EMPTY_SPACE:
             return None
         else:
-            self.matrix[x][y] = EMPTY_SPACE
+            self.matrix[y][x] = EMPTY_SPACE
             return removed_item
     
     def find_object_with_properties(self, key_value_pairs: dict, loose_match: bool=False):
         """Searches the grid for objects that have all of the given key/value pairs in their data."""
-        """Returns a list of coordinates for all objects that match."""
+        """Returns a set of coordinates for all objects that match."""
         """If loose_match is enabled, objects that match any one of the key value pairs will be included."""
-        locations = []
+        locations = set()
 
         if loose_match:
-            for y in range(len(self.matrix)):
-                for x in range(len(self.matrix[y])):
-                    object = self.matrix[x][y]
+            for y in range(self.height):
+                for x in range(self.width):
+                    object = self.matrix[y][x]
                     for key in key_value_pairs:
                         if key in object and key_value_pairs[key] == object[key]:
-                            locations.append((x,y))
+                            locations.add((x,y))
                             break
         else:
             matches_required = len(key_value_pairs)
-            for y in range(len(self.matrix)):
-                for x in range(len(self.matrix[y])):
-                    object = self.matrix[x][y]
+            for y in range(self.height):
+                for x in range(self.width):
+                    object = self.matrix[y][x]
                     matches = 0
                     for key in key_value_pairs:
                         if key in object and key_value_pairs[key] == object[key]:
                             matches += 1
                     if matches == matches_required:
-                        locations.append((x,y))
+                        locations.add((y,x))
         
         return locations
 
