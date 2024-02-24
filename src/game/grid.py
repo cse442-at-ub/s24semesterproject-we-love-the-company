cd2e8bac8409e3463848602049085ee06b340638
ululu <mtmerlino@gmail.com>
2024-02-23 19:40:35 -0500

    refactoring to use dicts instead of strings

--- src/game/grid.py
+++ src/game/grid.py
@@ -1,4 +1,4 @@
-EMPTY_SPACE = '.'
+EMPTY_SPACE = dict()
 
 class Grid:
     def __init__(self, width: int, height: int):
@@ -13,35 +13,26 @@ class Grid:
         for y in range(height):
             self.matrix.append([EMPTY_SPACE] * width)
     
-    def __str__(self):
-        row_strings = []
-        for row in self.matrix:
-            row_strings.append(''.join(row))
-        return '\n'.join(row_strings)
-    
     def is_inbounds(self, x: int, y: int):
         """Checks if the given pair of coordinates are inside the bounds of the grid."""
         return x >= 0 and y >= 0 and x < self.width and y < self.height
     
-    def insert(self, item: str, x: int, y: int):
-        """Places an object at the given coordinates. Replaces anything that was already there."""
-        """Returns True if the item was placed successfully, or False if an idential item was already present."""
-        if len(item) != 1:
-            raise ValueError(f"Attempting to insert item '{item}' with invalid length ({len(item)}). Expected length of exactly 1.")
-        elif item == EMPTY_SPACE:
+    def insert(self, item: dict, x: int, y: int):
+        """Places an object at the given coordinates."""
+        """Returns True if the placement was successful, and False if something is already in that space."""
+        if item == EMPTY_SPACE:
             raise ValueError(f"Attempting to insert item '{EMPTY_SPACE}' which normally denotes empty space. Use removal functions to place empty space.")
         elif not self.is_inbounds(x,y):
             raise ValueError(f"Attempting to insert item '{item}' at out of bounds grid position ({x},{y}).")
         
-        replaced_item = self.matrix[x][y]
-        if replaced_item == item:
-            return False
-        else:
+        if self.matrix[x][y] == EMPTY_SPACE:
             self.matrix[x][y] = item
             return True
+        else:
+            return False
     
     def remove_at_location(self, x: int, y: int):
-        """Removes the object at the given coordinates, and returns the object."""
+        """Removes an object at the given coordinates, and returns the object."""
         """If the space was already empty, returns None instead."""
         if not self.is_inbounds(x,y):
             raise ValueError(f"Attempting to remove item at out of bounds grid position ({x},{y}).")
@@ -52,21 +43,44 @@ class Grid:
         else:
             self.matrix[x][y] = EMPTY_SPACE
             return removed_item
+    
+    def find_object_with_properties(self, key_value_pairs: dict, loose_match: bool=False):
+        """Searches the grid for objects that have all of the given key/value pairs in their data."""
+        """Returns a list of coordinates for all objects that match."""
+        """If loose_match is enabled, objects that match any one of the key value pairs will be included."""
+        locations = []
 
-    def remove_all_of_type(self, item: str):
-        """Searches the grid for all copies of a given object and removes them."""
-        """Returns a list of tuples of coordinates where the objects used to be."""
-        if len(item) != 1:
-            raise ValueError(f"Attempting to remove item '{item}' with invalid length ({len(item)}). Expected length of exactly 1.")
-        elif item == EMPTY_SPACE:
-            raise ValueError(f"Attempting to remove item '{EMPTY_SPACE}' which normally denotes empty space.")
+        if loose_match:
+            for y in range(len(self.matrix)):
+                for x in range(len(self.matrix[y])):
+                    object = self.matrix[x][y]
+                    for key in key_value_pairs:
+                        if key in object and key_value_pairs[key] == object[key]:
+                            locations.append((x,y))
+                            break
+        else:
+            matches_required = len(key_value_pairs)
+            for y in range(len(self.matrix)):
+                for x in range(len(self.matrix[y])):
+                    object = self.matrix[x][y]
+                    matches = 0
+                    for key in key_value_pairs:
+                        if key in object and key_value_pairs[key] == object[key]:
+                            matches += 1
+                    if matches == matches_required:
+                        locations.append((x,y))
         
-        removed_locations = []
-        for y in range(len(self.matrix)):
-            row = self.matrix[y]
-            for x in range(len(row)):
-                if self.matrix[x][y] == item:
-                    self.matrix[x][y] = EMPTY_SPACE
-                    removed_locations.append((x,y))
+        return locations
+
+    def remove_all_with_properties(self, key_value_pairs: dict, loose_match: bool=False):
+        """Removes all objects that have the given properties, searching and selecting the same way as find_object_with_properties."""
+        """Returns a list of all objects removed."""
+        targets = self.find_object_with_properties(key_value_pairs,loose_match)
+        removed = []
+        for target in targets:
+            removed.append(self.remove_at_location(target[0],target[1]))
         
-        return removed_locations
+        return removed
+
+if __name__ == "__main__":
+    print("Tests go here!")
