b33aecb487507f706a3872cdf6fde6d19a2bc684
ululu <mtmerlino@gmail.com>
2024-02-27 14:48:27 -0500

    naming consistency

--- src/test/cache.py
+++ src/test/cache.py
@@ -31,13 +31,13 @@ class AssetCacheTests(unittest.TestCase):
         self.assertEqual(AssetCache.flush_image_cache(),2)
         self.assertEqual(AssetCache.flush_image_cache(),0)
     
-    def test_cache_sound(self):
+    def test_cache_audio(self):
         AssetCache.get_audio("src/game/Assets/button_click.mp3")
         self.assertIn("src/game/Assets/button_click.mp3",AssetCache.audio_cache)
         self.assertTrue(AssetCache.is_audio_cached("src/game/Assets/button_click.mp3"))
         self.assertFalse(AssetCache.is_audio_cached("src/game/Assets/Background_music_menu.wav"))
     
-    def test_uncache_sound(self):
+    def test_uncache_audio(self):
         AssetCache.get_audio("src/game/Assets/button_click.mp3")
         AssetCache.uncache_audio("src/game/Assets/button_click.mp3")
         self.assertNotIn("src/game/Assets/button_click.mp3",AssetCache.audio_cache)
