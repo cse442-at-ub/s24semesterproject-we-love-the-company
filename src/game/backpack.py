f99586a7bfc1ab8fe98cbf5436b5f04fdaf011c8
Sploder12 <trevorschupbach@gmail.com>
2024-02-27 22:41:24 -0500

    player items

--- src/game/backpack.py
+++ src/game/backpack.py
@@ -1,4 +1,4 @@
-class BackPack:
+class Backpack:
 
     def __init__(self, capacity):
         self.capacity = capacity
@@ -7,53 +7,60 @@ class BackPack:
 
     def add(self, id):
         self.number = 0
-        count = sum(self.dict.values())
-        if len(self.dict) > 0:
+        if not self.isEmpty():
             if type(self.dict.get(id)) == type(self.number):
                 self.number = self.dict.get(id)
-        if count < self.capacity:
+
+        if not self.isFull():
             self.number = self.number + 1
             self.dict.update({id: self.number})
 
     def remove(self,id):
         self.number = 0
-        if len(self.dict) > 0:
+        if not self.isEmpty():
             if type(self.dict.get(id))==type(self.number) :
                 self.number = self.dict.get(id)
             self.number = self.number - 1
+
         self.dict.update({id: self.number})
         if self.number <= 0:
             self.dict.pop(id)
+
+    def isFull(self):
+        return sum(self.dict.values()) >= self.capacity
+    
+    def isEmpty(self):
+        return len(self.dict) <= 0
 
     def printItems(self):
         print(self.dict)
 
 if __name__ == "__main__":
     ## Test 1
-    backpack1 = BackPack(5)
+    backpack1 = Backpack(5)
     backpack1.add("common")
     backpack1.printItems()
 
     ## Test 2
-    backpack2 = BackPack(5)
+    backpack2 = Backpack(5)
     backpack2.remove("")
     backpack2.printItems()
 
     ## Test 3
-    backpack3 = BackPack(5)
+    backpack3 = Backpack(5)
     backpack3.add("common")
     backpack3.remove("")
     backpack3.printItems()
 
     ## Test 4
-    backpack4 = BackPack(5)
+    backpack4 = Backpack(5)
     backpack4.add("common")
     backpack4.add("common")
     backpack4.remove("common")
     backpack4.printItems()
 
     ## Test 5
-    backpack5 = BackPack(6)
+    backpack5 = Backpack(6)
     backpack5.add("common")
     backpack5.add("common")
     backpack5.remove("common")
@@ -61,7 +68,7 @@ if __name__ == "__main__":
     backpack5.printItems()
 
     ## Test 6
-    backpack6 = BackPack(5)
+    backpack6 = Backpack(5)
     backpack6.add("common")
     backpack6.add("common")
     backpack6.add("common")
@@ -71,14 +78,14 @@ if __name__ == "__main__":
     backpack6.printItems()
 
     ## Test 7
-    backpack7 = BackPack(5)
+    backpack7 = Backpack(5)
     backpack7.add("common")
     backpack7.add("common")
     backpack7.add("uncommon")
     backpack7.printItems()
 
     ## Test 8
-    backpack8 = BackPack(2)
+    backpack8 = Backpack(2)
     backpack8.add("common")
     backpack8.add("common")
     backpack8.add("uncommon")
--- src/game/player.py
+++ src/game/player.py
@@ -19,9 +19,12 @@ class Player:
     def position(self):
         return (self.x, self.y)
 
+    # returns False when movement isn't possible
     def move(self, x, y, grid: Grid):
         if (grid.is_inbounds(self.x + x, self.y + y)):
             obj = grid.get_object(self.x + x, self.y + y)
+
+            # SUBJECT TO CHANGE !!!
             if ("name" not in obj or obj["name"] != "wall"):
                 self.x += x
                 self.y += y
@@ -31,12 +34,82 @@ class Player:
 
     def moveLeft(self, grid: Grid):
         return self.move(-1, 0, grid)
-    
+
     def moveDown(self, grid: Grid):
         return self.move(0, 1, grid)
-    
+
     def moveUp(self, grid: Grid):
         return self.move(0, -1, grid)
-    
+
     def moveRight(self, grid: Grid):
         return self.move(1, 0, grid)
+
+    def useHeld(self):
+        pass
+
+    # picks up an item from the ground
+    def pickUp(self, x, y, grid: Grid, range = 1):
+        # check if out of range
+        if (abs(self.x - x) + abs(self.y - y) > range):
+            return False
+
+        if (self.heldItem != None):
+            if (grid.is_inbounds(x, y)):
+                obj = grid.remove_at_location(x, y)
+                if (obj != None):
+                    self.heldItem = obj
+                    return True
+
+        return False
+
+    # puts item in hands into inventory
+    def stash(self):
+        if (self.heldItem != None and not self.inventory.isFull()):
+            self.inventory.add(self.heldItem)
+            self.heldItem = None
+            return True
+
+        return False
+
+    # drops currently held item to feet
+    def drop(self, grid: Grid):
+        if (self.heldItem != None):
+            if (grid.insert(self.heldItem, self.x, self.y)):
+                self.heldItem = None
+                return True
+
+        return False
+
+    # drops from the backpack
+    def dropFromBackpack(self, id, grid: Grid):
+        if (not self.inventory.isEmpty() and id in self.inventory.dict):
+            if (grid.insert(id)):
+                self.inventory.remove(id)
+                return True
+
+        return False
+
+    # takes item from inventory and puts into hands
+    def retrieve(self, id):
+        if (self.heldItem == None and not self.inventory.isEmpty()):
+            if (id in self.inventory.dict):
+                self.heldItem = id
+                self.inventory.remove(id)
+                return True
+
+        return False
+
+    # returns "defeated" or new hitDie
+    def getHit(self):
+        res = strike.downgrade_die(self.hitDie)
+
+        if (res != "defeated"):
+            self.hitDie = res
+
+        return res
+
+    # increases the die
+    def increaseDie(self):
+        self.hitDie = strike.upgrade_die(self.hitDie)
+
+
